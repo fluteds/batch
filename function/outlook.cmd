dd15a13f1ba49d21761604a5f1284e3e0f9eb8fb
fluteds <hi@dont-ban.me>
2022-02-15 15:57:10 +0000

    Delete outlook.cmd

--- function/outlook.cmd
+++
@@ -1,9 +0,0 @@
-:: Opens Outlooks localdata then deletes the Internet Calendars
-
-@echo off
-echo Running this script WILL delete your Outlook calendars.
-echo Are you sure you want to continue?
-pause 
-cd "%LOCALAPPDATA%\Microsoft\Outlook\"
-del subscriptions.pst
-pause
